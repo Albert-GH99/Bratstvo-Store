c9875eb6e3aef61b32308ba065481093f582276b
Albert-GH99 <albertlionel99@gmail.com>
2025-06-23 23:23:55 +0800

    Add files via upload
    Initial working code

---
+++ src/index.js
@@ -0,0 +1,11 @@
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import App from './App'; // Mengimport komponen utama App dari App.js
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+    
